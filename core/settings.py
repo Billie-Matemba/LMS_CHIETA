616602fe2939103dd336cedd17fdd2b69a0609d3
Billie-Matemba <billiematemba133@gmail.com>
2026-01-19 22:58:36 +0200

    final_Push

--- core/settings.py
+++ core/settings.py
@@ -467,8 +467,8 @@ AUTHENTICATION_BACKENDS = [
 
 # Email configuration 
 # 🚨 TEMPORARY DEMO CHANGE - Real email sending for demo
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
